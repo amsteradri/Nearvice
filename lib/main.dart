0b6b69b190e6ef3ec7cce3f5344552cd556e2e8f
amsteradri <amsteradri@gmail.com>
2024-04-11 12:01:57 +0200

    finished main page responsive , modified button size and text formfield size

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
-import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-
-
 
 Color brokenWhite = Color(0xFFFFFEEE); // Esto representa un blanco con un toque de gris
 
@@ -47,127 +44,128 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
     final screenWidth = MediaQuery.of(context).size.width;
-    final double beginX = screenWidth / sqrt(screenWidth * screenWidth + screenHeight * screenHeight) * 5.5;
-    final double beginY = -screenHeight / sqrt(screenWidth * screenWidth + screenHeight * screenHeight) * 0.2;
-    final double endX = screenWidth / sqrt(screenWidth * screenWidth + screenHeight * screenHeight) * 1.2;
-    final double endY = screenHeight / sqrt(screenWidth * screenWidth + screenHeight * screenHeight) * 1.2;
-    final logoSize = screenHeight * 0.224;
-    final double padding = screenWidth * 0.1;
-    final double formWidth = screenWidth - padding * 2;
+    final padding = screenWidth * 0.1;
+    final formWidth = screenWidth - padding * 2;
+
+    // Calcula el tamaño del logo como una fracción de la altura de la pantalla para mantenerlo responsivo.
+    final logoSize = screenHeight * 0.15;
 
     return Scaffold(
-      resizeToAvoidBottomInset: false, // Añadido para evitar que el contenido se mueva hacia arriba
       body: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
-            begin: Alignment(beginX, beginY),
-            end: Alignment(endX, endY),
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
             colors: [brokenWhite, Colors.black],
             stops: [0.35, 0.35],
           ),
         ),
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: padding),
-          child: Center(
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: formWidth,
-                    height: logoSize * 1.3,
-                    child: Transform.translate(
-                      offset: Offset(0, screenHeight * 0.117),
-                      child: Transform.rotate(
-                        angle: 0.617,
-                        child: Image.asset('assets/images/logo.png'),
-                      ),
+          child: Column(
+            children: [
+              Expanded(
+                flex: 2,
+                child: Center(
+                  child: Transform.translate(
+                    // Calcula el offset como un porcentaje de la altura de la pantalla
+                    offset: Offset(0, screenHeight * 0.075), // Por ejemplo, mueve el logo un 8% hacia abajo.
+                    child: Transform.rotate(
+                      angle: 0.48, // Rotación ajustada, si es necesaria.
+                      child: Image.asset('assets/images/logo.png', height: logoSize),
                     ),
                   ),
-                  const SizedBox(height: 170),
-                  Container(
-                    width: formWidth,
-                    child: TextFormField(
-                      decoration: const InputDecoration(
-                        hintText: 'Nombre de usuario',
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 16),
-                  Container(
-                    width: formWidth,
-                    child: TextFormField(
-                      decoration: const InputDecoration(
-                        hintText: 'Contraseña',
-                      ),
-                      obscureText: true, // Correctamente movido aquí
-                    ),
-                  ),
-                  const SizedBox(height: 24),
-                  Container(
-                    width: formWidth,
-                    child: ElevatedButton(
-                      onPressed: () {}, // Acción al presionar
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all(Colors.black),
-                        foregroundColor: MaterialStateProperty.all(Colors.white),
-                      ),
-                      child: const Text('Iniciar sesión'),
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  Container(
-                    width: formWidth,
-                    child: OutlinedButton(
-                      onPressed: () {}, // Acción al presionar
-                      style: ButtonStyle(
-                        foregroundColor: MaterialStateProperty.all(Colors.white),
-                        side: MaterialStateProperty.all(BorderSide(color: Colors.white)),
-                      ),
-                      child: const Text('Registrarse'),
-                    ),
-                  ),
-                  const SizedBox(height: 16),
-                  Container(
-                    width: formWidth,
-                    child: TextButton(
-                      onPressed: () {}, // Acción al presionar
-                      style: ButtonStyle(
-                        foregroundColor: MaterialStateProperty.all(Colors.white),
-                      ),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min, // Ajusta al contenido
-                        children: [
-                          SvgPicture.asset(
-                            'assets/images/google.svg', // Asegúrate de que esta es la ruta correcta
-                            width: 20,
-                            height: 20,
-                          ),
-                          const SizedBox(width: 8),
-                          const Text('Continuar con Google'),
-                        ],
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  Container(
-                    width: formWidth,
-                    child: TextButton.icon(
-                      onPressed: () {}, // Acción al presionar
-                      icon: const Icon(Icons.apple, color: Colors.white),
-                      label: const Text('Continuar con Apple'),
-                      style: ButtonStyle(
-                        foregroundColor: MaterialStateProperty.all(Colors.white),
-                      ),
-                    ),
-                  ),
-                ],
+                ),
               ),
-            ),
+
+              SizedBox(height: 20),
+              Expanded(
+                flex: 3,
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 25), // Aumenta este valor para hacer los campos de texto menos anchos
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            hintText: 'Nombre de usuario',
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 25), // Ajusta este valor según necesites
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            hintText: 'Contraseña',
+                          ),
+                          obscureText: true,
+                        ),
+                      ),
+
+                      SizedBox(height: 24),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(Colors.black),
+                          foregroundColor: MaterialStateProperty.all(Colors.white),
+                          padding: MaterialStateProperty.all(EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0)), // Aumenta el padding
+                          textStyle: MaterialStateProperty.all(TextStyle(fontSize: 16)), // Aumenta el tamaño del texto
+                          minimumSize: MaterialStateProperty.all(Size(180, 50)), // Establece un tamaño mínimo para el botón
+                        ),
+                        child: const Text('Iniciar sesión'),
+                      ),
+                      SizedBox(height: 8),
+                      OutlinedButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                          foregroundColor: MaterialStateProperty.all(Colors.white),
+                          side: MaterialStateProperty.all(BorderSide(color: Colors.white)),
+                          padding: MaterialStateProperty.all(EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0)), // Aumenta el padding
+                          textStyle: MaterialStateProperty.all(TextStyle(fontSize: 16)), // Aumenta el tamaño del texto
+                          minimumSize: MaterialStateProperty.all(Size(180, 50)), // Establece un tamaño mínimo para el botón
+                        ),
+                        child: const Text('Registrarse'),
+                      ),
+                      SizedBox(height: 16),
+// Ajuste aquí para el icono de Google SVG.
+                      TextButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                          foregroundColor: MaterialStateProperty.all(Colors.white),
+                          padding: MaterialStateProperty.all(EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0)), // Aumenta el padding
+                          textStyle: MaterialStateProperty.all(TextStyle(fontSize: 16)), // Ajusta el tamaño del texto
+                          minimumSize: MaterialStateProperty.all(Size(180, 50)), // Establece un tamaño mínimo para el botón
+                        ),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            SvgPicture.asset('assets/images/google.svg', width: 24, height: 24), // Ajusta el tamaño si es necesario
+                            SizedBox(width: 8),
+                            Text('Continuar con Google'),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      TextButton.icon(
+                        onPressed: () {},
+                        icon: Icon(Icons.apple, color: Colors.white),
+                        label: Text('Continuar con Apple'),
+                        style: ButtonStyle(
+                          foregroundColor: MaterialStateProperty.all(Colors.white),
+                          padding: MaterialStateProperty.all(EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0)), // Aumenta el padding
+                          textStyle: MaterialStateProperty.all(TextStyle(fontSize: 16)), // Aumenta el tamaño del texto
+                          minimumSize: MaterialStateProperty.all(Size(180, 50)), // Establece un tamaño mínimo para el botón
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
     );
   }
 }
-
